62bd67217c2ede0e28c391343e89173948b96ab1
Mirko-A <adzicmirko97@gmail.com>
2024-08-15 00:54:35 +0200

    Update neovim config

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,13 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        opts = {
-            lazy = true,
-            transparent = true,
-            styles = {
-                sidebars = "transparent",
-                floats = "transparent",
-            },
-        },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      lazy = true,
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
     },
+  },
 }
