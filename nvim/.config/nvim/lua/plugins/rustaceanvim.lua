2bd3cecb9babd807eeb27c3fd16c00ee03a4b467
Mirko-A <adzicmirko97@gmail.com>
2025-01-18 13:39:59 +0100

    clean up nvim cfg a little

--- nvim/.config/nvim/lua/plugins/rustaceanvim.lua
+++ nvim/.config/nvim/lua/plugins/rustaceanvim.lua
@@ -5,55 +5,27 @@ return {
     ft = { "rust" },
     opts = {
       server = {
-        on_attach = function(_, bufnr)
-          vim.keymap.set("n", "<leader>cR", function()
-            vim.cmd.RustLsp("codeAction")
-          end, { desc = "Code Action", buffer = bufnr })
-          vim.keymap.set("n", "<leader>dr", function()
-            vim.cmd.RustLsp("debuggables")
-          end, { desc = "Rust Debuggables", buffer = bufnr })
-        end,
         default_settings = {
           -- rust-analyzer language server configuration
-          ["rust-analyzer"] = {
-            cargo = {
-              allFeatures = true,
-              loadOutDirsFromCheck = true,
-              buildScripts = {
-                enable = true,
-              },
+          -- Disable inlay hints
+          inlayHints = {
+            typeHints = false,
+            parameterHints = false,
+          },
+          procMacro = {
+            enable = true,
+            ignored = {
+              -- Removed `async_trait` for now, can be brought back if needed.
+              ["async-trait"] = {},
+              ["napi-derive"] = { "napi" },
+              ["async-recursion"] = { "async_recursion" },
             },
-            -- Add clippy lints for Rust.
-            checkOnSave = true,
-            -- Disable inlay hints
-            inlayHints = {
-              typeHints = false,
-              parameterHints = false,
-            },
-            procMacro = {
-              enable = true,
-              ignored = {
-                -- Removed `async_trait` for now, can be brought back if needed.
-                ["async-trait"] = {},
-                ["napi-derive"] = { "napi" },
-                ["async-recursion"] = { "async_recursion" },
-              },
-            },
-            rustfmt = {
-              extraArgs = { "+nightly" },
-            },
+          },
+          rustfmt = {
+            extraArgs = { "+nightly" },
           },
         },
       },
     },
-    config = function(_, opts)
-      vim.g.rustaceanvim = vim.tbl_deep_extend("force", vim.g.rustaceanvim or {}, opts or {})
-      if vim.fn.executable("rust-analyzer") == 0 then
-        LazyVim.error(
-          "**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/",
-          { title = "rustaceanvim" }
-        )
-      end
-    end,
   },
 }
