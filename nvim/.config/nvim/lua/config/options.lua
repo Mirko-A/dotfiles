30070933ee40ef30707a2395434f228fd997a99b
Mirko-A <adzicmirko97@gmail.com>
2025-02-17 23:33:18 +0100

    indentation settings in vim + update plugin versions

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Indentation
+vim.o.expandtab = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+
 -- Disable animations
 vim.g.snacks_animate = false
 vim.g.minipairs_disable = true
