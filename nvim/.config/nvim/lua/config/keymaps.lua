0e17b360f41e90360982a29cc0dbea59f73a012d
Mirko-A <adzicmirko97@gmail.com>
2025-04-18 22:42:26 +0200

    vim leave insert mode with jk

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,5 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Leave insert mode with jj
-vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true })
+-- Leave insert mode with jk
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
+
+-- Marks
+vim.keymap.set({ "n" }, "<leader>mc", "<cmd>delm! | delm A-Z0-9<cr>", { desc = "Clear all marks", remap = true })
