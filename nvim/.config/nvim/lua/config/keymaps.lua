51bfee15de83189013ebf45435ee9729ba654540
Mirko-A <adzicmirko97@gmail.com>
2025-04-25 16:23:10 +0200

    vim: custom keybinds

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,5 +5,13 @@
 -- Leave insert mode with jk
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
 
--- Marks
-vim.keymap.set({ "n" }, "<leader>mc", "<cmd>delm! | delm A-Z0-9<cr>", { desc = "Clear all marks", remap = true })
+local custom_keymaps = {
+  -- Clipboard
+  { mode = { "n", "v" }, key = "p", cmd = '"0p', desc = "Paste last yank" },
+  -- Marks
+  { mode = { "n" }, key = "c", cmd = "<cmd>delm! | delm A-Z0-9<cr>", desc = "Clear all marks" },
+}
+
+for _, map in ipairs(custom_keymaps) do
+  vim.keymap.set(map.mode, "<leader>m" .. map.key, map.cmd, { desc = map.desc })
+end
