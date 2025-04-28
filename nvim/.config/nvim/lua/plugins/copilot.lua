de7e2cc062cc5e86697c7884021e9f4ba4cb932d
Mirko-A <adzicmirko97@gmail.com>
2025-04-28 13:46:59 +0200

    vim: disable copilot auto trigger of suggestions

---
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,12 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    suggestion = {
+      auto_trigger = false,
+      keymap = {
+        next = "<M-n>",
+        prev = "<M-p>",
+      },
+    },
+  },
+}
