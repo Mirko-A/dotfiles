e05a8e45477ef7fccfa6d3d4ecd89d3d2ece5073
Mirko-A <adzicmirko97@gmail.com>
2025-08-22 16:20:22 +0200

    vim: cleanup

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -4,22 +4,5 @@ return {
     oldfiles = {
       include_current_session = true,
     },
-    previewers = {
-      builtin = {
-        syntax_limit_b = 1024 * 100, -- 100KB
-      },
-      -- grep = {
-      --   rg_glob = true, -- enable glob parsing
-      --   glob_flag = "--iglob", -- case insensitive globs
-      --   glob_separator = "%s%-%-", -- query separator pattern (lua): ' --'
-      -- },
-    },
-  },
-  keys = {
-    -- find
-    { "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
-    { "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
-    { "<leader>fr", LazyVim.pick("oldfiles", { cwd = vim.uv.cwd() }), desc = "Recent (cwd)" },
-    { "<leader>fR", "<cmd>FzfLua oldfiles<cr>", desc = "Recent" },
   },
 }
