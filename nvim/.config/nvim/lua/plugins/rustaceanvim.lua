cbe5b22454fce58b075dd6022b49ce3fd26b040d
Mirko-A <adzicmirko97@gmail.com>
2025-08-29 12:26:14 +0200

    vim: rust plugin refactor

--- nvim/.config/nvim/lua/plugins/rustaceanvim.lua
+++ nvim/.config/nvim/lua/plugins/rustaceanvim.lua
@@ -1,37 +1,15 @@
 return {
-  {
-    "mrcjkb/rustaceanvim",
-    ft = { "rust" },
-    opts = {
-      server = {
-        default_settings = {
-          -- rust-analyzer language server configuration
-          ["rust-analyzer"] = {
-            cargo = {
-              allFeatures = false,
-              -- add features that rust-analyzer should consider enabled
-              features = {},
-            },
-            -- Disable inlay hints
-            inlayHints = {
-              typeHints = false,
-              parameterHints = false,
-            },
-            procMacro = {
-              enable = true,
-              ignored = {
-                -- Removed `async_trait` for now, can be brought back if needed.
-                -- ["async-trait"] = { "async-trait" },
-                ["napi-derive"] = { "napi" },
-                ["async-recursion"] = { "async_recursion" },
-              },
-            },
-            rustfmt = {
-              extraArgs = { "+nightly" },
-            },
-          },
-        },
-      },
-    },
-  },
+  "mrcjkb/rustaceanvim",
+  opts = function(_, opts)
+    local rustAnalyzerSettings = opts.server.default_settings["rust-analyzer"]
+    rustAnalyzerSettings.cargo.allFeatures = false
+    rustAnalyzerSettings.cargo.features = {}
+    rustAnalyzerSettings.inlayHints = {
+      typeHints = false,
+      parameterHints = false,
+    }
+    rustAnalyzerSettings.rustfmt = {
+      extraArgs = { "+nightly" },
+    }
+  end,
 }
