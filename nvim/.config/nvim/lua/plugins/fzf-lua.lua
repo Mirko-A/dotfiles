3a10d7106acb71521e6e8d4e95ab8e0a215824d5
Mirko-A <adzicmirko97@gmail.com>
2025-10-08 19:52:18 +0200

    vim: limit previewer in fzf-lua

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -4,5 +4,10 @@ return {
     oldfiles = {
       include_current_session = true,
     },
+    previewers = {
+      builtin = {
+        syntax_limit_b = 1024 * 100, -- 100KB
+      },
+    },
   },
 }
