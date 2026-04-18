a79dda60fb8d0473fae8c972f26c7952fa050fa0
Mirko Adzic <adzicmirko97@gmail.com>
2026-04-18 13:48:07 +0200

    nvim: comment out indentation autocmd

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -2,11 +2,12 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "lua", "javascript", "typescript", "javascriptreact", "typescriptreact" },
-  callback = function()
-    vim.bo.shiftwidth = 2
-    vim.bo.tabstop = 2
-    vim.bo.softtabstop = 2
-  end,
-})
+-- Not needed anymore.
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+--   pattern = { "lua", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+--   callback = function()
+--     vim.bo.shiftwidth = 2
+--     vim.bo.tabstop = 2
+--     vim.bo.softtabstop = 2
+--   end,
+-- })
