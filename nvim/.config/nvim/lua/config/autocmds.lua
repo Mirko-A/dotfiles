43d7caf9ca6ef621423e49fc8407627536c6fda2
Mirko Adzic <adzicmirko97@gmail.com>
2026-04-29 09:35:36 +0200

    nvim: add indendation autocmd back in

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -2,12 +2,11 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Not needed anymore.
--- vim.api.nvim_create_autocmd({ "FileType" }, {
---   pattern = { "lua", "javascript", "typescript", "javascriptreact", "typescriptreact" },
---   callback = function()
---     vim.bo.shiftwidth = 2
---     vim.bo.tabstop = 2
---     vim.bo.softtabstop = 2
---   end,
--- })
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "lua", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+  end,
+})
