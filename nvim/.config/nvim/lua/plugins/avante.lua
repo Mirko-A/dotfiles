98837075145dfdda908e6a04e88181d9f6eb668e
Mirko Adzic <adzicmirko97@gmail.com>
2026-04-29 11:24:15 +0200

    nvim: set avante provider to local ollama

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -1,4 +1,28 @@
 return {
   "yetone/avante.nvim",
-  opts = {},
+  opts = {
+    provider = "ollama",
+    providers = {
+      ollama = {
+        __inherited_from = "openai",
+        endpoint = "http://localhost:11434/v1",
+        api_key_name = "",
+        model = "unsloth/Qwen3.5-35B-A3B-GGUF:UD-Q4_K_XL",
+        timeout = 30000,
+      },
+    },
+  },
+  keys = {
+    { "<leader>aa", "<cmd>AvanteAsk<CR>", desc = "Ask Avante", mode = { "n", "v", "x" } },
+    { "<leader>ac", "<cmd>AvanteChat<CR>", desc = "Chat with Avante" },
+    { "<leader>ae", "<cmd>AvanteEdit<CR>", desc = "Edit Avante", mode = { "n", "v", "x" } },
+    { "<leader>af", "<cmd>AvanteFocus<CR>", desc = "Focus Avante" },
+    { "<leader>ah", "<cmd>AvanteHistory<CR>", desc = "Avante History" },
+    { "<leader>am", "<cmd>AvanteModels<CR>", desc = "Select Avante Model" },
+    { "<leader>an", "<cmd>AvanteChatNew<CR>", desc = "New Avante Chat" },
+    { "<leader>ap", "<cmd>AvanteSwitchProvider<CR>", desc = "Switch Avante Provider" },
+    { "<leader>ar", "<cmd>AvanteRefresh<CR>", desc = "Refresh Avante" },
+    { "<leader>as", "<cmd>AvanteStop<CR>", desc = "Stop Avante" },
+    { "<leader>at", "<cmd>AvanteToggle<CR>", desc = "Toggle Avante" },
+  },
 }
