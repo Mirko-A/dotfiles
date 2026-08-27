4d991ab87ec8e95207056dd9008546f4c2b64d1e
Mirko Adzic <adzicmirko97@gmail.com>
2026-08-28 01:15:48 +0200

    nvim: json file two space indents

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -3,7 +3,7 @@
 -- Add any additional autocmds here
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "lua", "javascript", "typescript", "javascriptreact", "typescriptreact" },
+  pattern = { "lua", "javascript", "typescript", "javascriptreact", "typescriptreact", "json" },
   callback = function()
     vim.bo.shiftwidth = 2
     vim.bo.tabstop = 2
